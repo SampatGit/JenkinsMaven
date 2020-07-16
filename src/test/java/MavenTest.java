085e74e553293d17b68c61c4bdf7e0843cd28687
sampatgit <sampatkumarsahu24@gmail.com>
2020-07-16 20:59:53 +0530

    sonar test

--- src/test/java/MavenTest.java
+++ src/test/java/MavenTest.java
@@ -1,13 +1,23 @@
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class MavenTest {
 	
-	public static void main(String[] args) {
+	@Test
+	public void unitTest(){
+		int a = 10;	
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.google.com");
+		driver.get("http://www.makemytrip.com");
+		System.out.println("title"+ driver.getTitle());
+		System.out.println("title"+ driver.getTitle());
+		System.out.println("title"+ driver.getTitle());
+		Assert.assertTrue(driver.getTitle().contains("MakeMyTrip"));
 		
 	}
-
 }
+
+
